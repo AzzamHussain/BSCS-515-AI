a65cc6d980ae2c687d3b52019f417bcfb67b75ae
Azzam Hussain <126112158+AzzamHussain@users.noreply.github.com>
2024-01-28 16:00:19 +0500

    corrected

--- Task06.py
+++ Task06.py
@@ -1,7 +1,12 @@
-list=['Azzam','yaseen','Huzaifa','laraib','Mustafa']
-newList=[]
-for elements in list:
-    if elements.islower():
-        newList.append(elements)
+#(i)Write a list comprehension which, from a list, generates a lowercased version of each string that has 
+#length greater than five.
 
-print(newList)
+list=['Azzam','yaseen','Huzaifa','laraib','Mustafa']
+#newList=[]
+new_list = [element.lower() for element in list if len(element) > 5]
+
+# for elements in list:
+#     if elements.islower():
+#         newList.append(elements)
+
+print(new_list)
